3ae4f2516b3e6932d64afdaf269e2f586ab8d0bb
LeelooE <leeloonita@gmail.com>
2018-07-02 13:39:36 -0600

    Combined template for reducers made

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,44 @@
 import { createStore, combineReducers } from 'redux';
 
+//Add_expense, remove_expense, edit_expense, set_text_filter, sort_by_date, sort_by_amout, set_start_date, set_end_date
+
+//Expenses Reducer
+
+
+const expensesReducerDefaultState = []
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch(action.type){
+        default:
+        return state;
+    }
+};
+
+//Filters Reducer
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+    switch(action.type){
+        default:
+        return state;
+    }
+};
+
+//Store Creation
+
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
         id: 'asldkfa;jsldjf',
